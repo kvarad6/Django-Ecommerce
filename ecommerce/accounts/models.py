e1afac480f7b369a8193aa76877ee9913e9394a1
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-28 16:51:55 +0530

    created sendEmailToken signal

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base.models import BaseModel
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from base.emails import sendAccountActivationEmail
+import uuid
 
 # Create your models here.
 
@@ -11,3 +15,14 @@ class Profile(BaseModel):
     is_email_verified = models.BooleanField(default=False)
     email_token = models.CharField(max_length=100, null=True, blank=True)
     profile_image = models.ImageField(upload_to="profile")
+
+
+@receiver(post_save, sender=User)
+def sendEmailToken(sender, instance, created, **kwargs):
+    try:
+        if created:
+            email_token = str(uuid.uuid4())
+            email = instance.email
+            sendAccountActivationEmail(email, email_token)
+    except Exception as e:
+        print(e)
