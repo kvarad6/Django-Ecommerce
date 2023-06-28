67345fb456d6453bdafd68e1ba72e8c8a3e3ff6f
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-28 16:55:06 +0530

    added email configurations

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -132,3 +132,10 @@ STATICFILES_DIR = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'varad.kulkarni@searce.com'
+EMAIL_HOST_PASSWORD = ''
