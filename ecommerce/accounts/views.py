e3303ad447cc9d9b97a9b58ac1fa72b419ae65e9
varad-kulkarni-searce <varad.kulkarni@searce.com>
2023-06-28 16:53:23 +0530

    added register and login_page functions

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
 
 
 def login_page(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username=email)
+
+        if not user_obj.exists():
+            messages.warning(request, "User not exists")
+            return HttpResponseRedirect(request.path_info)
+
+        if not user_obj[0].profile.is_email_verified:
+            messages.warning(request, "Please verify the email")
+            return HttpResponseRedirect(request.path_info)
+
+        user_obj = authenticate(username=email, password=password)
+        if user_obj:
+            login(request, user_obj)
+            return HttpResponseRedirect('/')
+
+        messages.warning(request, 'Invalid credentials')
+        return HttpResponseRedirect(request.path_info)
     return render(request, 'accounts/login.html')
 
 
@@ -13,4 +39,19 @@ def register_page(request):
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         password = request.POST.get('password')
+
+        user_obj = User.objects.filter(username=email)
+
+        if user_obj.exists():
+            messages.warning(request, "Email is already taken.")
+            return HttpResponseRedirect(request.path_info)
+
+        user_obj = User.objects.create(
+            first_name=first_name, last_name=last_name, email=email, username=email)
+        user_obj.set_password(password)
+        user_obj.save()
+
+        messages.warning(request, 'An email has been send successfully')
+        return HttpResponseRedirect(request.path_info)
+
     return render(request, 'accounts/register.html')
